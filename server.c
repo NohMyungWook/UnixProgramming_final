c123c263277543f7a0992a76bcc043b33185574d
NohMyungWook <jeongas9810@naver.com>
2022-12-10 17:29:35 +0900

    server change

--- server.c
+++ server.c
@@ -23,7 +23,7 @@ int main(){
     struct sockaddr_un ser, cli;
     int sd, nsd, len, clen;
     struct cli_item cli_item;
-    struct item items[20];
+    struct item items[50];
     int items_num = 0;
 
     unlink(SOCKET_NAME);
@@ -41,36 +41,44 @@ int main(){
         perror("bind");
         exit(1);
     }
-
-    if(listen(sd, 5) < 0){
-        perror("listen");
-        exit(1);
-    }
-
-    printf("Waiting...\n");
-    if((nsd = accept(sd, (struct sockaddr *)&cli, &clen)) == -1){
-        perror("accept");
-        exit(1);
-    }
-
-    if(recv(nsd, (struct cli_item*)&cli_item, sizeof(cli_item), 0) == -1){
-        perror("recv");
-        exit(1);
-    }
-    else{
-        strcpy(items[items_num].have, cli_item.have);
-        strcpy(items[items_num].want, cli_item.want);
-        items[items_num].addr = cli;
-        if(write(nsd, buf, strlen(buf))== -1){
-            perror("write");
+    while(1){
+        if(items_num > 50){
+            printf("Cannot add item in list\n");
+            items_num--;
+        }
+        else{
+            if(listen(sd, 5) < 0){
+            perror("listen");
             exit(1);
+            }
+
+            printf("Waiting...\n");
+            if((nsd = accept(sd, (struct sockaddr *)&cli, &clen)) == -1){
+                perror("accept");
+                exit(1);
+            }
+
+            if(recv(nsd, (struct cli_item*)&cli_item, sizeof(cli_item), 0) == -1){
+                perror("recv");
+                exit(1);
+            }
+            else{
+                strcpy(items[items_num].have, cli_item.have);
+                strcpy(items[items_num].want, cli_item.want);
+                items[items_num].addr = cli;
+            }
+
+            printf("Received want: %s\n", items[items_num].want);
+            printf("Received have: %s\n", items[items_num].have);
+            items_num++;
+        }
+        for(int i=0; i<items_num; i++){
+            if(send(nsd, (struct item*)&items[i], sizeof(items[i]), 0)== -1){
+                perror("send");
+                exit(1);
+            }
         }
     }
-
-
-    printf("Received want: %s\n", items[items_num].want);
-    printf("Received have: %s\n", items[items_num].have);
-    printf("Received addr: %d\n", items[items_num].addr.sun_family);
     close(nsd);
     close(sd);
 }
