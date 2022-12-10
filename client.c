29895d5b9b1de5fa050fe8ad157492ce983061b8
KYJ <thjbjs@outlook.com>
2022-12-11 00:31:34 +0900

    make fork branch

--- client.c
+++ client.c
@@ -19,6 +19,19 @@ struct SendPacket
     struct AddItem nowItem;
 };
 
+int sendStruct(int clientSD, struct AddItem newItem, int no){
+	//보낼 패킷을 만들기
+    struct SendPacket newPacket;
+    newPacket.no = no;
+    newPacket.nowItem = newItem;
+    
+    if (send(clientSD, (struct SendPacket*) &newPacket, sizeof(newPacket), 0) == -1) {
+        perror("send");
+        return 0;
+    }
+	return 1;
+}
+
 int getCount(int sd){
     int nowCount;
     if(recv(sd, (int* )&nowCount, sizeof(nowCount), 0) == -1){
@@ -29,14 +42,6 @@ int getCount(int sd){
         return nowCount;
     }
     return -1;
-}
-
-int sendStruct(int clientSD, struct SendPacket* newPacket, int bufSize){
-	if (send(clientSD, (struct SendPacket*) newPacket, bufSize, 0) == -1) {
-        perror("send");
-        return 0;
-    }
-	return 1;
 }
 
 int get1Item(int sd, int count, struct AddItem* allItemList){
@@ -60,12 +65,15 @@ int get1Item(int sd, int count, struct AddItem* allItemList){
 
 int printNowList(int sd, struct AddItem* allItemList){
     printf("-----현재 물물교환 가능 리스트-----\n");
+
     int itemInt = getCount(sd);
-    if (itemInt >= 0){
+
+    if (itemInt > 0){
         get1Item(sd, itemInt, allItemList);
     }else{
         printf("현재 물물교환이 가능한 물건이 없습니다..\n");
     }
+
     printf("---------------------------------------\n");
     return itemInt;
 }
@@ -79,12 +87,8 @@ int createNew(int sd){
     printf("가지고 있는 물건을 입력하세요 : ");
     scanf("%s", newItem.have);
 
-    //보낼 패킷을 만들기
-    struct SendPacket newPacket;
-    newPacket.no = 2;
-    newPacket.nowItem = newItem;
+    sendStruct(sd, newItem, 2);
 
-    sendStruct(sd, &newPacket, sizeof(struct SendPacket));
     printf("\n등록되었습니다. \n");
     return 1;
 }
@@ -124,6 +128,10 @@ int main() {
         scanf("%d", &taskNum);
 
         if(taskNum == 1){
+            if(nowListSize <=0){
+                printf("현재 물물교환이 가능한 물건이 없습니다");
+                continue;
+            }
             printf("교환을 원하시는 번호를 입력해주세요 : ");
             int changeNum = 0;
             scanf("%d", &changeNum);
@@ -133,13 +141,9 @@ int main() {
                 struct AddItem sendItem;
                 strcpy(sendItem.have, allItemList[changeIndex].have);
                 strcpy(sendItem.want, allItemList[changeIndex].want);
-                
-                //보낼 패킷을 만들기
-                struct SendPacket newPacket;
-                newPacket.no = 1;
-                newPacket.nowItem = sendItem;
 
-                sendStruct(sd, &newPacket, sizeof(struct SendPacket));
+                sendStruct(sd, sendItem, 1);
+
                 printf("\n교환이 완료되었습니다.\n");
 
             }else{
