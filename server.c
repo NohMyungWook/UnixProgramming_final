0c4e9db14fb814c67614bc4ad62a9508d2898895
NohMyungWook <jeongas9810@naver.com>
2022-12-10 20:30:27 +0900

    server

--- server.c
+++ server.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <signal.h>
 
 #define SOCKET_NAME "market"
 
@@ -12,18 +14,44 @@ struct item{
     char have[50];
 };
 
-struct cli_item{
-    char want[50];
-    char have[50];
+struct user_input{
+    int no;
+    struct item item;
 };
 
+void send_item_list(int nsd, int items_num, struct item *items){
+    send(nsd, &items_num, sizeof(items_num), 0);
+    for(int i=0; i<items_num; i++){
+        if(send(nsd, (struct item *)(items + i), sizeof(struct item), 0)== -1){
+            perror("send");
+            exit(1);
+        }
+    }
+}
+
+bool add_item(struct user_input *user_input, struct item *items, int items_num){
+    if(items_num > 50){
+        printf("Cannot add item\n");
+        return false;
+    }
+    strcpy(items[items_num].have, user_input->item.have);
+    strcpy(items[items_num].want, user_input->item.want);
+    return true;
+}
+
+void delete_item(struct item *items, int n, int items_num){
+    for(int i=n; i<items_num; i++){
+        memcpy(&items[i], &items[i+1], sizeof(struct item));
+    }
+}
 
 int main(){
     char buf[256] = "test";
     struct sockaddr_un ser, cli;
     int sd, nsd, len, clen;
-    struct cli_item cli_item;
+    struct item item;
     struct item items[50];
+    struct user_input user_input;
     int items_num = 0;
 
     unlink(SOCKET_NAME);
@@ -40,6 +68,7 @@ int main(){
     strcpy(items[items_num].have, "have2");
     items_num++;
     //end of test 
+
     memset((char*)&ser, 0, sizeof(struct sockaddr_un));
     ser.sun_family = AF_UNIX;
     strcpy(ser.sun_path, SOCKET_NAME);
@@ -49,49 +78,45 @@ int main(){
         perror("bind");
         exit(1);
     }
-    while(1){
-        if(items_num > 50){
-            printf("Cannot add item in list\n");
-            items_num--;
-        }
-        else{
-            if(listen(sd, 5) < 0){
-            perror("listen");
-            exit(1);
-            }
+    if(listen(sd, 5) < 0){
+    perror("listen");
+    exit(1);
+    }
 
-            printf("Waiting...\n");
-            if((nsd = accept(sd, (struct sockaddr *)&cli, &clen)) == -1){
-                perror("accept");
-                exit(1);
-            }
-            else{
-                send(nsd, &items_num, sizeof(items_num), 0);
+    printf("Waiting for Client...\n");
+    if((nsd = accept(sd, (struct sockaddr *)&cli, &clen)) == -1){
+        perror("accept");
+        exit(1);
+    }
+    send_item_list(nsd, items_num, items);
+    while(1){
+        if(recv(nsd, (struct user_input*)&user_input, sizeof(user_input), 0) == -1){
+            perror("recv");
+            exit(1);
+        }
+        switch(user_input.no){
+            case 1:
                 for(int i=0; i<items_num; i++){
-                    printf("%d\n", i);
-                    printf("%s, %s\n", items[i].have, items[i].want);
-                    if(send(nsd, (struct item *)(items + i), sizeof(struct item), 0)== -1){
-                        perror("send");
-                        exit(1);
+                    if(strcmp(items[i].want, user_input.item.want) && strcmp(items[i].have, user_input.item.have)){
+                        delete_item(items, i, items_num);
+                        items_num --;
+                    }
+                    else{
+                        printf("Not found\n");
+                    }
+                    for(int i=0; i<items_num; i++){
+                        printf("%s", items[i].have);
                     }
                 }
-            }
-
-            if(recv(nsd, (struct cli_item*)&cli_item, sizeof(cli_item), 0) == -1){
-                perror("recv");
-                exit(1);
-            }
-            else{
-                strcpy(items[items_num].have, cli_item.have);
-                strcpy(items[items_num].want, cli_item.want);
-                
-            }
-
-            printf("Received want: %s\n", items[items_num].want);
-            printf("Received have: %s\n", items[items_num].have);
-            items_num++;
+                send_item_list(nsd, items_num, items);
+                break;
+            case 2:
+                if(add_item(&user_input, items, items_num)){
+                    items_num++;
+                }
+                send_item_list(nsd, items_num, items);
+                break;
         }
-        
     }
     close(nsd);
     close(sd);
